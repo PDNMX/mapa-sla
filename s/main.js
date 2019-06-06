b9a02672dfe1a0688890799ffdbcdaf8eb46e813
Sergio Rodríguez <sergio.rdzsg@gmail.com>
2019-06-06 18:08:21 -0500

    agregando tile

--- s/main.js
+++ s/main.js
@@ -1,12 +1,16 @@
 // see Observable US Map https://observablehq.com/@d3/u-s-map
 
 let projection = d3.geoMercator()
-    .scale(1200)
+    .scale(1100)
     .center([-102.34034978813841, 24.012062015793]);
 let path = d3.geoPath(projection);
 //var projection = d3.geoAlbersUsa().scale(1280).translate([480, 300]); // use this if you have lon,lat
 let map = d3.select('#mexico');
 let mexico;
+
+let tile = d3.tile()
+    .scale(projection.scale() * 2 * Math.PI)
+    .translate(projection([0, 0]));
 
 let tip = d3.tip()
       .attr('class', 'd3-tip')
@@ -33,15 +37,31 @@ d3.json("mexico.json")
             /* console.log(mexico); */
             return(mexico);
         }).then(function(mexico){
-            map.append('path').attr('id', 'step-all')
-            .attr('vector-effect', 'non-scaling-stroke')
-            .attr('d', path(topojson.feature(mexico, mexico.objects.collection)));
-            /* var states_filter = function(a, b){return (a !== b);} */
+            var tiles = tile();
+            var defs = map.append("defs");
+
+            defs.append('path').attr('id', 'step-all')
+                .attr('vector-effect', 'non-scaling-stroke')
+                .attr('d', path(topojson.feature(mexico, mexico.objects.collection)));
+
+            map.append("g")
+                .attr("clip-path", "url(#clip)")
+                .selectAll("image")
+                .data(tiles)
+                .enter().append("image")
+                .attr("xlink:href", function(d) { return "http://" + "abc"[d[1] % 3] + ".tile.openstreetmap.org/" + d[2] + "/" + d[0] + "/" + d[1] + ".png"; })
+                .attr("width", Math.round(tiles.scale))
+                .attr("height", Math.round(tiles.scale))
+                .attr("x", function(d) { return Math.round((d[0] + tiles.translate[0]) * tiles.scale); })
+                .attr("y", function(d) { return Math.round((d[1] + tiles.translate[1]) * tiles.scale); });
+        
+            map.append("use")
+                .attr("xlink:href", "#land")
+                .attr("class", "stroke");
+
             map.append('path').attr('id', 'step-0').attr('opacity', 0)
                 .attr('stroke-width', 0).attr('vector-effect','non-scaling-stroke')
                 .attr('d', path(topojson.mesh(mexico, mexico.objects.collection)));
-                // for `topojson.mesh` see also https://github.com/topojson/topojson-client/blob/master/README.md#mesh
-            /* var counties_filter = function(a, b) { return (a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0)); }; */
             map.append('path').attr('id', 'step-1')
                 .attr('stroke', '#aaa').attr('opacity', 0)
                 .attr('stroke-width', 0).attr('vector-effect','non-scaling-stroke')
